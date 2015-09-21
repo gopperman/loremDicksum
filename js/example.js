370be4371d5ca4d2d1d16f9a13770a52a0bee74f
Greg Opperman <gopperman@gmail.com>
2015-09-20 22:40:06 -0400

    Lets try a click listener

--- js/example.js
+++ js/example.js
@@ -1,9 +1,11 @@
 $( document ).ready(function (){
-	var markup = '<h2 class="example-dicksum"></h2>';
-	var i = $('input[name="paragraphs"]').val();
-	while( i-- ) {
-		markup += '<p class="example-dicksum"></p>';
-	}
-	$( '#example' ).empty().append( markup );
-	$( '.example-dicksum').loremDicksum();
+	$( '#generate').click( function() {
+		var markup = '<h2 class="example-dicksum"></h2>';
+		var i = $('input[name="paragraphs"]').val();
+		while( i-- ) {
+			markup += '<p class="example-dicksum"></p>';
+		}
+		$( '#example' ).empty().append( markup );
+		$( '.example-dicksum').loremDicksum();
+	} );
 } );
