66a1fa1c21e8016a20be7c4448f8944525ded463
Greg Opperman <gopperman@gmail.com>
2015-09-20 22:37:14 -0400

    Generator js

---
+++ js/example.js
@@ -0,0 +1,9 @@
+$( document ).ready(function (){
+	var markup = '<h2 class="example-dicksum"></h2>';
+	var i = $('input[name="paragraphs"]').val();
+	while( i-- ) {
+		markup += '<p class="example-dicksum"></p>';
+	}
+	$( '#example' ).empty().append( markup );
+	$( '.example-dicksum').loremDicksum();
+} );
